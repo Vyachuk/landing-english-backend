d877b1e0be0ba22ef7f4857ff35b4781338b2e25
Vyachuk <Slavkoka045@gmail.com>
2023-11-06 13:27:57 +0200

    ADD: validate regEXP

--- Schemas/validate.js
+++ Schemas/validate.js
@@ -1,8 +1,10 @@
 const Joi = require("joi");
 
+const { emailRegexp } = require("../constants/sendEmail");
+
 const validateSchema = Joi.object({
   name: Joi.string().required(),
-  email: Joi.string().required(),
+  email: Joi.string().pattern(emailRegexp).required(),
 });
 
 module.exports = {
