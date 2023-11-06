cf62178f56027733ed3ab01bc89561b2f4519c79
Vyachuk <Slavkoka045@gmail.com>
2023-11-06 11:13:24 +0200

    EDIT: proj config

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require("express");
+
+const cors = require("cors");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(cors());
+app.use(express.json());
+
+app.post("/", async (req, res, next) => {
+  res.json({ ...req.body });
 });
 
-app.listen(3000, () => {
-  console.log("Example app listening on port 3000!");
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
 });
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message });
+});
+
+module.exports = app;
