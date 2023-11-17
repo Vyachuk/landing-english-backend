b987c9e365da6906a23b360ed067d2cf49a1ea51
Vyachuk <Slavkoka045@gmail.com>
2023-11-17 18:50:42 +0200

    EDIT: validate pattern

--- helpers/validateBody.js
+++ helpers/validateBody.js
@@ -4,9 +4,7 @@ const validateBody = (schema) => {
   const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
     if (error) {
-      return next(
-        HttpError("400", "Email fails to match the required pattern")
-      );
+      return next(HttpError("400", error.message));
     }
     next();
   };
